b271c0d73f3ab265a0f6ad3b75f869fd932ae429
Michael Wedl <michael@syslifters.com>
2026-04-29 13:55:42 +0200

    cleanupfiles: remove unreferenced files from filesystem

--- api/src/sysreptor/management/commands/cleanupfiles.py
+++ api/src/sysreptor/management/commands/cleanupfiles.py
@@ -1,13 +1,22 @@
+import argparse
 import logging
 
 from django.core.management.base import BaseCommand
 from django.db import transaction
+from django.db.models import F
 
+from sysreptor.api_utils.backup_utils import walk_storage_dir
 from sysreptor.utils.files import get_all_file_fields
+from sysreptor.utils.utils import groupby_to_dict
 
 
 class Command(BaseCommand):
-    help = 'Clean up file entries from the DB where the files do not exist on the fielsystem.'
+    help = 'Clean up unused files.'
+
+    def add_arguments(self, parser) -> None:
+        parser.add_argument('--database', action=argparse.BooleanOptionalAction, default=True, help='Clean up files from the database')
+        parser.add_argument('--filesystem', action=argparse.BooleanOptionalAction, default=True, help='Clean up files from the filesystem')
+        parser.add_argument('--storage', action='store', default=None, help='Clean up files from a specific storage')
 
     def file_exists(self, f):
         try:
@@ -16,9 +25,19 @@ class Command(BaseCommand):
         except Exception:
             return False
 
+    def handle(self, database=False, filesystem=False, storage=None, *args, **options):
+        if database:
+            self.cleanup_database(only_storage=storage)
+        if filesystem:
+            self.cleanup_filesystem(only_storage=storage)
+
     @transaction.atomic
-    def handle(self, *args, **options):
+    def cleanup_database(self, only_storage=None):
+        logging.info('Cleaning up file references from the database that no longer exist on the filesystem')
         for field_info in get_all_file_fields():
+            if only_storage and only_storage != field_info['storage_name']:
+                continue
+
             logging.info(f'Cleaning up {field_info["model"]._meta.label}.{field_info["field_name"]} in storage {field_info["storage_name"]}')
             qs = field_info['model'].objects \
                 .filter(pk__in=[
@@ -33,3 +52,30 @@ class Command(BaseCommand):
                 qs.delete()
                 logging.info(f'  Deleted {qs.count()} entries')
 
+    def cleanup_filesystem(self, only_storage=None):
+        logging.info('Cleaning up files from the filesystem that are not referenced in the database')
+        for storage_name, fields in groupby_to_dict(get_all_file_fields(), key=lambda f: f['storage_name']).items():
+            if only_storage and only_storage != storage_name:
+                continue
+
+            storage = fields[0]['storage']
+            logging.info(f'Cleaning up files for storage {storage_name}')
+
+            query_parts = []
+            for field_info in fields:
+                query_parts.append(field_info['model'].objects.annotate(file_path=F(field_info['field_name'])).values('file_path'))
+                if hasattr(field_info['model'], 'history'):
+                    query_parts.append(field_info['model'].history.annotate(file_path=F(field_info['field_name'])).values('file_path'))
+            fs_files = set(walk_storage_dir(storage))
+            db_files = set(query_parts[0].union(*query_parts[1:]).values_list('file_path', flat=True))
+
+
+            unreferenced_files = fs_files - db_files
+            for f in unreferenced_files:
+                try:
+                    storage.delete(f)
+                except FileNotFoundError:
+                    pass
+                except Exception as ex:
+                    logging.warning(f'  Could not delete {f}: {ex}')
+            logging.info(f'  Deleted {len(unreferenced_files)} files')
